1ae181b338a2f34f6a30bed906d77ea9badd03a8
Alexey Ushakov <alexey.ushakov@deliveryhero.com>
2021-04-19 11:44:44 +0200

    Reproduce crash

--- TestLibCoreCrashUITests/TestLibCoreCrashUITests.swift
+++ TestLibCoreCrashUITests/TestLibCoreCrashUITests.swift
@@ -7,12 +7,21 @@
 
 import XCTest
 
+func grey_customMatcher() -> GREYMatcher {
+  let mathcesBlock: GREYMatchesBlock = { (element: Any) in
+    return false
+  }
+  let descriptionBlock: GREYDescribeToBlock = { $0.appendText("grey_customMatcher") }
+  return GREYElementMatcherBlock(matchesBlock: mathcesBlock, descriptionBlock: descriptionBlock)
+}
+
 class TestLibCoreCrashUITests: XCTestCase {
 
   func testExample() {
-      let application: XCUIApplication = XCUIApplication()
-      application.launch()
-      EarlGrey.selectElement(with: grey_keyWindow())
-        .perform(grey_tap())
-    }
+    var error: NSError?
+    let application: XCUIApplication = XCUIApplication()
+    application.launch()
+    EarlGrey.selectElement(with: grey_accessibilityID("SOME_NONEXISTENT_ID"))
+      .assert(grey_customMatcher(), error: &error)
+  }
 }
